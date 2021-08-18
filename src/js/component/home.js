3b39b62fab1551bbe2308aeecdd29877ce8d4121
Allen St Fort <allenstfort1@gmail.com>
2021-08-18 01:22:18 +0000

    Fixed the delete & input text

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 export function Home() {
 	// const [isShown, setIsShown] = useState(false);
+	const [isShown, setIsShown] = useState({ state: false, index: undefined });
 	const [variable, setVariable] = useState(null);
 
 	const apiUrl =
@@ -29,9 +30,21 @@ export function Home() {
 
 	let todo = (variable || []).map((item, i) => {
 		return (
-			<li className="list-group-item" key={i}>
+			<li
+				className="col-12 list-group-item text-info"
+				key={i}
+				onMouseEnter={() => setIsShown({ state: true, index: i })}
+				onMouseLeave={() =>
+					setIsShown({ state: false, index: undefined })
+				}>
 				{item.label}
-				<button onClick={() => removeItem(i)}>X</button>
+				{isShown.state && isShown.index === i && (
+					<button
+						className="float-right"
+						onClick={() => removeItem(i)}>
+						x
+					</button>
+				)}
 			</li>
 		);
 	});
@@ -68,19 +81,24 @@ export function Home() {
 	};
 
 	return (
-		<div className="List ">
-			<h1 className="title">To Do List</h1>
-			<input
-				className={"input"}
-				onKeyDown={newTodo}
-				type="text"
-				id="fname"
-				placeholder="Task"
-				name="fname"></input>
-			<div>
-				<ul className="list-group">{todo}</ul>
-				<div className="footer">{todo.length} item left</div>
+		<>
+			<h1 className="text-center text-success mt-4 ">To Do List</h1>
+			<div className="List col-4  mx-auto list-group mt-4">
+				<input
+					className="col-12 p-3 font-weight-bold text-center"
+					onKeyDown={newTodo}
+					type="text"
+					id="fname"
+					placeholder="Enter A Task"
+					name="fname"
+				/>
+				<ul className="col-12 p-0">
+					{todo}
+					<span className="footer list-group-item text-danger">
+						{todo.length} item left
+					</span>
+				</ul>
 			</div>
-		</div>
+		</>
 	);
 }
